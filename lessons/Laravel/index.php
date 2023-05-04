c22befaaf029626aaa1d716d1ad70d3ebff966ea
Deha <ssibn@mail.ru>
2023-05-04 23:18:41 +0400

    Laravel

---
+++ lessons/Laravel/index.php
@@ -0,0 +1,14 @@
+<?php
+    requare_once('first_page.php');
+
+    Route:::get('/', function (){
+        class User
+        return 'Hello Laravel';
+    })
+
+    Route:::get('/name', function (){
+        return 'Hello Laravel';
+    })
+    // Eloquent - ORM
+
+
