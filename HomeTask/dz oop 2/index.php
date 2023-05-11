89e6101a42862f02ea937ba891b6ccf6eac9c4b4
ssibn <ssibn@mail.ru>
2023-05-12 01:43:59 +0400

    dz oop 2

--- HomeTask/dz oop 2/index.php
+++ HomeTask/dz oop 2/index.php
@@ -5,7 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="./img/smile.png">
-    <link rel="stylesheet" href="./css/style.css">
+    <link href="./css/bootstrap.min.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
     
     <title>OOP 2</title>
 </head>
@@ -17,10 +18,17 @@
             {
                 public $name;
                 public $filters;
+                public $price;
                 public $listProducts;
 
-                function __construct($name = "", $filters = "Price"){
+                function __construct($name, $filters, $price, $listProducts){
                     $this -> name = $name;
+                    $this -> filters = $filters;
+                    $this -> price = $price;
+                    $this -> listProducts = $listProducts;
+                }
+                function _filters(){
+                    $this -> price;
                 }
             }
         ?>
@@ -28,111 +36,80 @@
     </div>
 
     <div class="task">
-    <h3>Task 2</h3>
-
+        <h3>Task 2</h3>
         <?php
         class PhoneCategory extends Category{
-            function __construct($filters = ["Ram", "CountSim", "Hdd", "OS"]){
-                Category:: __construct($filters);
-                //
+            public $phone;
+            public $ram;
+            public $countSim;
+            public $hdd;
+            public $os;
+            function __construct($ram, $countSim, $hdd, $os){
+                parent::__construct($filters, $price);
+                $this -> ram = $ram;
+                $this -> countSim = $CountSim;
+                $this -> hdd = $hdd;
+                $this -> os = $os;
+            }
+            function _filters(){
+                parent::_filters();
+                $this -> ram = $ram;
+                $this -> countSim = $CountSim;
+                $this -> hdd = $hdd;
+                $this -> os = $os;
             }
         }
         ?>
     </div>
 
     <div class="task">
-    <h3>Task 3</h3>
-
+        <h3>Task 3</h3>
         <?php
         class MonitorCategory extends Category{
-            function __construct($filters = ["Diagonal ", "Frequency"]){
-                Category:: __construct($filters);
-                //
+            public $diagonal;
+            public $frequency;
+            function __construct($diagonal, $frequency){
+                parent:: __construct($filters, $price);
+                $this -> diagonal = $diagonal;
+                $this -> frequency = $frequency;
+                
             }
-            function Show(){
-                return print_r($filters);
+            function _filters(){
+                parent::_filters();
+                $this -> diagonal = $diagonal;
+                $this -> frequency = $frequency;
             }
-
         }
-        $test = new MonitorCategory();
-        $test->Show();
         ?>
     </div>
 
-    <?php
+    <div class="task">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <h3 class="text-center">Task 4</h3>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-1">
+                    <h4>Category</h4>
+                </div>
+            </div>
+            <div class="row">
+            <select class="form-select" aria-label="Default select example">
+            <option selected>Open this select menu</option>
+            <option value="1">Phone</option>
+            <option value="2">Monitor</option>
+            </select>
+            </div>
+        </div>
+    </div>
 
-class User
-{
-  private $data = array();
+    
 
-  private $name;
-  protected $age;
-  public $salary;
-
-  public function __set($name, $value)
-  {
-    $this->data[$name] = $value;
-  }
-}
-
-$user = new User();
-$user->name = 'John';
-$user->age = 34;
-$user->salary = 4200.00;
-$user->message = 'hello';
-
-var_dump($user);
-
-
-
-class User1
-{
-  protected $name;
-  protected $age;
-
-  public function __construct($name, $age)
-  {
-    $this->name = $name;
-    $this->age = $age;
-  }
-
-  public function __toString()
-  {
-    return $this->name . ', age ' . $this->age;
-  }
-}
-
-$user = new User1('John', 34);
-echo $user;
-
-
-
-
-
-class Point
-{
-	public $x;
-  	public $y;
-	function __construct($x, $y)
-	{
-		$this->x=$x;
-		$this->y=$y;
-	}
-	function Show()
-	{
-		echo 'Vertex: ('.$this->x.','.$this->y.') <br/>';
-	}
-}
-$p=new Point (100,200);
-$p->Show();
-$jspoint=json_encode($p);
-echo $jspoint.'<br/>';
-file_put_contents('jsonpoint.txt',$jspoint);
-
-?>
 
      <h2>Как мог <img src="./img/smile.png" alt="">  За дизайн простите и ошибки если найдете</h2>
 
-    <script src="js/app.js"></script>
+    <script src="js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
